a46c137e708dd19fd2d48ce732b02ef2404336ec
sasori9969 <64680421+sasori9969@users.noreply.github.com>
2025-02-01 11:09:11 +0000

    b

--- pages/3_Auswertung.py
+++ pages/3_Auswertung.py
@@ -1,13 +1,13 @@
 import streamlit as st
-import json
-from utils import load_competitions, load_participants  # Importiere die Funktionen
+import pandas as pd
+from utils import load_competitions, load_participants
 
 st.title("Auswertung")
 
 competitions = load_competitions()
 participants = load_participants()
 
-if competitions and participants and "results" in participants:  # Sicherstellen, dass Daten vorhanden sind
+if competitions and participants and "results" in participants:
     selected_competition = st.selectbox("Wettbewerb auswählen", options=list(competitions.keys()))
 
     if st.button("Auswertung anzeigen"):
@@ -16,10 +16,13 @@ if competitions and participants and "results" in participants:  # Sicherstellen
 
             # Einzelwertung
             st.subheader("Einzelwertung")
-            individual_results = {}
+            individual_results = []
             for shooter, result in results.items():
-                individual_results[shooter] = result  # Hier könnte man das beste Ergebnis berücksichtigen
-            st.dataframe(individual_results)  # Anzeige als DataFrame
+                individual_results.append({"Name": shooter, "Ergebnis": result})
+            df_individual = pd.DataFrame(individual_results)
+            df_individual = df_individual.sort_values(by="Ergebnis", ascending=False)
+            df_individual.insert(0, "Platz", range(1, len(df_individual) + 1))
+            st.dataframe(df_individual.set_index("Platz"))  # Index-Spalte ausblenden
 
             # Mannschaftswertung
             st.subheader("Mannschaftswertung")
@@ -30,11 +33,15 @@ if competitions and participants and "results" in participants:  # Sicherstellen
                         team_results[team] = []
                     team_results[team].append(result)
 
-            # Durchschnittliche Mannschaftsergebnisse berechnen
-            average_team_results = {}
+            # Summe der Mannschaftsergebnisse berechnen
+            sum_team_results = []
             for team, team_results_list in team_results.items():
-                average_team_results[team] = sum(team_results_list) / len(team_results_list) if team_results_list else 0
-            st.dataframe(average_team_results)  # Anzeige als DataFrame
+                sum_team_results.append({"Mannschaft": team, "Ergebnis": sum(team_results_list)})
+            df_team = pd.DataFrame(sum_team_results)
+            df_team = df_team.sort_values(by="Ergebnis", ascending=False)
+            df_team.insert(0, "Platz", range(1, len(df_team) + 1))
+            st.dataframe(df_team.set_index("Platz"))  # Index-Spalte ausblenden
+
         else:
             st.error("Bitte Wettbewerb auswählen.")
 else:
