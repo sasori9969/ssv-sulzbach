b842b15120be06b26c213e1ee9422776597a14aa
sasori9969 <64680421+sasori9969@users.noreply.github.com>
2025-02-03 12:32:34 +0000

    b

--- pages/2_Ergebnisse erfassen.py
+++ pages/2_Ergebnisse erfassen.py
@@ -7,16 +7,22 @@ st.title("Ergebnisse eingeben")
 competitions = load_competitions()
 participants = load_participants()
 
-if competitions and participants and "shooters" in participants and "teams" in participants:  # Check for teams
+if competitions and participants and "shooters" in participants:
     selected_competition = st.selectbox("Wettbewerb auswählen", options=list(competitions.keys()))
 
-    # Team-basierte Eingabe
-    team_shooters = {}
-    for team_name, team_members in participants["teams"].items():
-        team_shooters[team_name] = [shooter for shooter in team_members if shooter in participants["shooters"]]
+    # Team-basierte Eingabe ODER separate Eingabe
+    input_mode = st.radio("Eingabemodus", options=["Mannschaft", "Einzelperson"])
 
-    selected_team = st.selectbox("Mannschaft auswählen", options=list(team_shooters.keys()))
-    selected_shooter = st.selectbox("Schützen auswählen", options=team_shooters[selected_team]) # Filter shooters by team
+    if input_mode == "Mannschaft" and "teams" in participants:
+        team_shooters = {}
+        for team_name, team_members in participants["teams"].items():
+            team_shooters[team_name] = [shooter for shooter in team_members if shooter in participants["shooters"]]
+
+        selected_team = st.selectbox("Mannschaft auswählen", options=list(team_shooters.keys()))
+        selected_shooter = st.selectbox("Schützen auswählen", options=team_shooters[selected_team])
+    else:  # Einzelperson
+        selected_shooter = st.selectbox("Schützen auswählen", options=list(participants["shooters"].keys()))
+
     result = st.number_input("Ergebnis eingeben")
 
     if st.button("Ergebnis speichern"):
@@ -33,24 +39,29 @@ if competitions and participants and "shooters" in participants and "teams" in p
         else:
             st.error("Bitte Wettbewerb und Schützen auswählen.")
 
-    # Ergebnistabelle anzeigen (mit Team-Sortierung)
+    # Ergebnistabelle anzeigen (mit Team-Sortierung und Anzeige aller Schützen)
     st.subheader("Ergebnisse")
     if "results" in participants and selected_competition in participants["results"]:
         results_table = participants["results"][selected_competition]
 
-        # Sortierung nach Team und dann Ergebnis
+        # Sortierung nach Team (falls vorhanden) und dann Ergebnis
         sorted_shooters = []
-        for team_name in sorted(team_shooters.keys()):  # Teams in alphabetischer Reihenfolge
-            team_members = [shooter for shooter in team_shooters[team_name] if shooter in results_table]
-            sorted_team_members = sorted(team_members, key=lambda x: results_table[x], reverse=True)
-            sorted_shooters.extend(sorted_team_members)
+        if "teams" in participants:
+            for team_name in sorted(participants["teams"].keys()):
+                team_members = [shooter for shooter in participants["teams"][team_name] if shooter in results_table or shooter in participants["shooters"]] # Alle Schützen des Teams anzeigen
+                sorted_team_members = sorted(team_members, key=lambda x: results_table.get(x, -1), reverse=True) # Sortierung auch für Schützen ohne Ergebnis
+                sorted_shooters.extend(sorted_team_members)
+
+        # Hinzufügen von Schützen ohne Team (und ohne Ergebnis) am Ende
+        shooters_without_team = [shooter for shooter in participants["shooters"] if shooter not in sorted_shooters]
+        sorted_shooters.extend(shooters_without_team)
 
         for shooter in sorted_shooters:
-            result = results_table[shooter]
-            team_name = next((team for team, members in participants["teams"].items() if shooter in members), "Kein Team") # Teamzuordnung
-            col1, col2, col3, col4 = st.columns(4)  # Zusätzliche Spalte für Team
+            result = results_table.get(shooter, "") # Leeres Ergebnis für neue Schützen
+            team_name = next((team for team, members in participants["teams"].items() if shooter in members), "Kein Team")
+            col1, col2, col3, col4 = st.columns(4)
             with col1:
-                st.write(team_name) # Team anzeigen
+                st.write(team_name)
             with col2:
                 st.write(shooter)
             with col3:
@@ -65,4 +76,4 @@ if competitions and participants and "shooters" in participants and "teams" in p
         st.info("Noch keine Ergebnisse für diesen Wettbewerb.")
 
 else:
-    st.warning("Bitte erst Wettbewerbe, Schützen und Mannschaften anlegen.")
+    st.warning("Bitte erst Wettbewerbe und Schützen anlegen.")
